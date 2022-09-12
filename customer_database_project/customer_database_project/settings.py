ce826e621b1786794d532ce4719fe2cb8b8cd786
Spawney <44836744+Spawney@users.noreply.github.com>
2022-09-12 10:41:34 +0100

    Update settings.py

--- customer_database_project/customer_database_project/settings.py
+++ customer_database_project/customer_database_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3em4n(4o^ml-#$ioh7opn0%zbzay(31qldu49%63ich*xey__g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['synxcustomerdatabase.herokuapp.com']
 
@@ -118,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+DEBUG = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

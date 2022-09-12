b19f0f170938acbfc3a0c879dce62ec8f1cf4545
Spawney <44836744+Spawney@users.noreply.github.com>
2022-09-12 10:42:36 +0100

    Update settings.py

--- customer_database_project/customer_database_project/settings.py
+++ customer_database_project/customer_database_project/settings.py
@@ -118,13 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-DEBUG = True
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

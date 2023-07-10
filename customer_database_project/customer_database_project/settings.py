66ffa672481a79604d4f5d1a3f8e273727603b1d
Chris Pressley <chris.pressley@synx.com>
2023-07-10 07:32:31 +0100

    Test Checkin for pipeline run

--- customer_database_project/customer_database_project/settings.py
+++ customer_database_project/customer_database_project/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
+
+Test checkin for pipeline run
 """
 
 from pathlib import Path
